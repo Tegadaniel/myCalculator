763f0038b76e175e513c2185c9117018fa20790b
Philip Daniel Oghenetega <tegadaniels8@gmail.com>
2021-04-11 11:45:01 +0100

    commit

---
+++ myscripts.js
@@ -0,0 +1,7 @@
+function valuecal(result){
+    formCalculator.evalresult.value = formCalculator.evalresult.value + result;
+}
+function clr()
+{
+    document.getElementById("result").value = ""
+}
